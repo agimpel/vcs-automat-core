6342fd61799fe9e7a09c9aa83415141251a4206c
agimpel <agimpel@student.ethz.ch>
2018-07-29 19:16:11 +0200

    fixed parent module inclusion

--- unit_tests/vcs_connector.py
+++ unit_tests/vcs_connector.py
@@ -1,5 +1,7 @@
-import sys
-sys.path.append('..')
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
 import logging
 import time
 import os.path
