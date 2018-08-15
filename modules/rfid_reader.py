b958238f9e7c78331ff92d87f0e6eb8b9ed9ea01
agimpel <agimpel@student.ethz.ch>
2018-08-15 23:43:22 +0200

    switched to python-evdev for rfid reader, finalised implementation

--- modules/rfid_reader.py
+++ modules/rfid_reader.py
@@ -1,10 +1,18 @@
+import os
+import configparser
 from threading import Thread
 import logging
 import queue
 import binascii
 import time
+import evdev
+import asyncio
 
-import modules.PN532 as PN532
+from modules import CFG, DB
+
+# Name of the RFID reader. Find via devices=[evdev.InputDevice(path) for path in evdev.list_devices()]; for device in devices: print(device.path, device.name, device.phys)
+RFID_USB_NAME = 'OEM RFID Device (Keyboard)' 
+
 
 
 class RFID_Reader(Thread):
@@ -24,12 +32,31 @@ class RFID_Reader(Thread):
         self.rfid_queue = queue.Queue()
         self.is_running = False
 
-        # configuration of Adafruit PN532 for RaspberryPi
-        self.pn532 = PN532.PN532("/dev/ttyS0")
-        self.pn532.begin()
-        ic, ver, rev, support = self.pn532.get_firmware_version()
-        self.logger.info('Found Adafruit PN532 with firmware version: {}.{}'.format(ver, rev))
-        self.pn532.SAM_configuration()
+        # read stamp for rfid validation from config file
+        self.read_cfg(os.path.join(CFG, "rfid.cfg"))
+
+        # set up RFID device by identification of its name
+        devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+        if RFID_USB_NAME not in [device.name for device in devices]:
+            self.logger.error('RFID reader not found in devices. Try finding it with sudo and set up its permissions via udev rule')
+            self.reader = None
+            return
+        
+        path = devices[[device.name for device in devices].index(RFID_USB_NAME)].path
+        self.logger.info('Found RFID reader at '+path)
+        try:
+            self.reader = evdev.InputDevice(path)
+        except Exception as e:
+            self.logger.error('Could not open RFID reader.')
+            self.reader = None
+            return
+
+        # get exclusive read on device via EVIOCGRAB
+        self.reader.grab()
+        self.flush()
+        self.logger.info('Successfully connected, grabbed and flushed the RFID reader. Listening.')
+
+
 
     # run
     # INFO:     checks RFID reader for RFID tags, and if one is found, queues the corresponding UID for the worker
@@ -40,22 +67,63 @@ class RFID_Reader(Thread):
 
         while self.is_running:
             try:
-                uid = self.pn532.read_passive_target()
-
-                # break 1: check if rfid detected
-                if uid is None:
-                    self.logger.debug('no rfid detected')
-                    continue
-                self.logger.debug('card with UID {} found'.format(binascii.hexlify(uid).decode('ascii')))
-
-                # push uid to rfid queue
-                self.rfid_queue.put(binascii.hexlify(uid).decode('ascii'))
+                raw_data = self.poll()
+                self.logger.debug('Processing raw data from rfid reader.')
+                rfid = self.validate(raw_data)
+                if rfid is not False:
+                    self.logger.debug('detected rfid: '+rfid)
+                    self.rfid_queue.put(rfid)
 
                 time.sleep(5)
 
             except Exception as e:
                 self.logger.exception("exception: {}".format(e))
                 continue
+
+
+
+    def poll(self):
+        self.flush()
+        self.logger.debug('Started polling ...')
+        data = ''
+        for event in self.reader.read_loop():
+            if event.type is evdev.ecodes.EV_KEY and event.value is 1 and event.code is not evdev.ecodes.ecodes['KEY_LEFTSHIFT']:
+                data += evdev.ecodes.keys[event.code]
+            if data.endswith('KEY_ENTERKEY_EKEY_NKEY_DKEY_ENTER'):
+                self.logger.debug('Found end of reader output.')
+                data = data.replace('KEY_ENTER', '\n')
+                data = data.replace('KEY_SPACE', ' ')
+                data = data.replace('KEY_', '')
+                return data
+
+
+    def flush(self):
+        while self.reader.read_one() != None:
+            self.logger.debug('Flushed.')
+            pass
+
+
+    def validate(self, raw_input):
+        if len(raw_input) < 50:
+            self.logger.debug('Input was too short.')
+            return False
+        if raw_input[0:5] != 'LEGIC':
+            self.logger.debug('RFID is not of type LEGIC, got '+raw_input[0:5])
+            return False
+        if raw_input[self.stamp_index:self.stamp_index+len(self.stamp)] != self.stamp:
+            self.logger.debug('RFID stamp was not correct, got '+raw_input[self.stamp_index:self.stamp_index+len(self.stamp)])
+            return False
+
+        self.logger.debug('RFID was valid.')
+        return raw_input[self.stamp_index+len(self.stamp):self.stamp_index+len(self.stamp)+6]
+
+
+    def read_cfg(self, cfg_path):
+        config = configparser.SafeConfigParser()
+        config.read(cfg_path)
+        self.stamp = str(config['rfid']['stamp'])
+        self.stamp_index = int(config['rfid']['index'])
+
 
     # name
     # INFO:
@@ -64,6 +132,8 @@ class RFID_Reader(Thread):
     def exit(self):
         self.logger.info("SHUTDOWN")
         self.is_running = False
+        self.reader.ungrab()
+        self.reader.close()
 
 
 # name
--- unit_tests/rfid.py
+++ unit_tests/rfid.py
@@ -1,120 +1,43 @@
-from threading import Thread
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
 import logging
-import queue
-import binascii
 import time
-import hid
+import os.path
+import signal
+from threading import Thread
+
+from modules.rfid_reader import RFID_Reader
+
+# general settings
+PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+CFG = os.path.join(PATH, "../config/")
+DB = os.path.join(PATH, "../database/")
 
 
+# set-up of general logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s\t%(levelname)s\t[%(name)s: %(funcName)s]\t%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[logging.FileHandler(PATH + "/main.log"), logging.StreamHandler()])
 
-class RFID_Reader(Thread):
-
-    numbers = {'39': 0, '30':1, '31':2, '32':3, '33':4, '34':5, '35':6, '36':7, '37':8, '38':9}
-
-    # name
-    # INFO:
-    # ARGS:
-    # RETURNS:
-    def __init__(self):
-        # set-up for logging of rfid. Level options: DEBUG, INFO, WARNING, ERROR, CRITICAL
-        self.loglevel = logging.DEBUG
-        self.logtitle = 'rfid'
-        self.logger = logging.getLogger(self.logtitle)
-        self.logger.setLevel(self.loglevel)
-
-        Thread.__init__(self, daemon=True)
-        self.rfid_queue = queue.Queue()
-        self.is_running = False
-
-        # configuration of RFID Reader
-        self.reader = hid.device()
-        self.reader.open(0x09d8, 0x0410)
-        self.reader.set_nonblocking(1)
+# set-up for logging of main. Level options: DEBUG, INFO, WARNING, ERROR, CRITICAL
+loglevel = logging.DEBUG
+logtitle = 'main'
+logger = logging.getLogger(logtitle)
+logger.setLevel(loglevel)
 
 
+logger.info('start rfid thread')
+rfid = RFID_Reader()
+rfid.start()
 
-    # run
-    # INFO:     checks RFID reader for RFID tags, and if one is found, queues the corresponding UID for the worker
-    # ARGS:     /
-    # RETURNS:  /
-    def run(self):
-        self.is_running = True
+try:  # try-block for KeyboardInterrupt
+    while True:
+        time.sleep(0.5)
 
-        while self.is_running:
-            try:
-                raw_data = self.poll()
-                if raw_data is not None:
-                    self.logger.debug('detected rfid: '+raw_data[16:22])
-
-                time.sleep(1)
-
-            except Exception as e:
-                self.logger.exception("exception: {}".format(e))
-                continue
-
-
-
-    def poll(self):
-        data = ''
-        last_input_time = time.time()
-        time_delta = 2 #s
-
-        while time.time() < last_input_time + time_delta:
-            d = self.reader.read(8)
-            if d and d[2] is not 0:
-                last_input_time = time.time()
-                if str(d[2]) in self.numbers:
-                    data += str(self.numbers[str(d[2])])
-        
-        if data is not '':
-            return data
-        else:
-            return None
-
-
-
-
-
-
-    # name
-    # INFO:
-    # ARGS:
-    # RETURNS:
-    def exit(self):
-        self.logger.info("SHUTDOWN")
-        self.is_running = False
-        self.reader.close()
-
-
-# name
-# INFO:
-# ARGS:
-# RETURNS:
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s\t%(levelname)s\t[%(name)s: %(funcName)s]\t%(message)s',
-                        datefmt='%Y-%m-%d %I:%M:%S')
-    rfid = RFID_Reader()
-    rfid.start()
-
-    try:  # try-block for KeyboardInterrupt
-        while True:
-            time.sleep(0.5)
-
-    except KeyboardInterrupt:  # on CTRL-C, stop all threads and shut down
-        rfid.exit()
-        if rfid.isAlive():
-            rfid.join(5.0)
-
-
-
-
-
-
-
-
-
-
-
-
-
+except KeyboardInterrupt:  # on CTRL-C, stop all threads and shut down
+    rfid.exit()
+    if rfid.isAlive():
+        rfid.join(5.0)
